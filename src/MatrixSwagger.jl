15de399b7680311c72f7c7a09e052e07182c3511
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2018-07-11 22:57:46 -0400

    Add more utility funcs for swagger schema

--- src/MatrixSwagger.jl
+++ src/MatrixSwagger.jl
@@ -10,4 +10,41 @@ function typename(path::AbstractString)::Symbol
     Symbol(join(titlecase.(splitpath[4:end]), ""))
 end
 
+# Need convenience function to turn types into symbols/expressions for `insertfield!`
+"Inserts field `a` of type `T` into a type expression."
+function insertfield!(ex::Expr, a::Symbol, T::Union{Symbol,Expr})
+    if ex.head == :type
+        push!(ex.args[end].args, :($a::$T))
+    else
+        throw(ArgumentError("Can only add field to concrete type expression"))
+    end#if
+end#function
+
+"""
+    juliatype(topleveltype::String[, nestedtypes...])::Type
+
+Return corresponding Julia type for a swagger type string.
+
+Throws ArgumentError for unknown types.
+"""
+function juliatype(s::AbstractString, args...)::Union{Symbol,Expr}
+    if s == "string"
+        :String
+    elseif s == "array" && !isempty(args)
+        :(Vector{$(juliatype(args...))})
+    elseif s == "object"
+        :(Dict{String,Any})
+    elseif s == "boolean"
+        :Bool
+    elseif s == "file"
+        :IOBuffer
+    elseif s == "integer"
+        :Int64
+    elseif s == "number"
+        :Float64
+    else
+        throw(ArgumentError("No known corresponding Julia type"))
+    end#if
+end#function
+
 end#module
--- test/MatrixSwagger.jl
+++ test/MatrixSwagger.jl
@@ -2,4 +2,16 @@ using MatrixClientProtocol: MatrixSwagger
 
 @testset "Utils" begin
     @test MatrixSwagger.typename("/_matrix/client/r0/my/fun/path") == :MyFunPath
+    @testset "insertfield!" begin
+        e = :(struct Foo; a::String end)
+        MatrixSwagger.insertfield!(e, :b, :Int64)
+        @test all(e.args[end].args[end-1:end] .== [:(a::String), :(b::Int64)])
+        MatrixSwagger.insertfield!(e, :c, :(Vector{Int64}))
+        @test all(e.args[end].args[end-2:end] .==
+                  [:(a::String), :(b::Int64), :(c::Vector{Int64})])
+    end
+    @testset "juliatype" begin
+        @test MatrixSwagger.juliatype("boolean") == :Bool
+        @test MatrixSwagger.juliatype("array", "object") == :(Vector{Dict{String,Any}})
+    end
 end
